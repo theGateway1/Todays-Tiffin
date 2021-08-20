364721f96ad35b04f4530ef38f97a484c75a9185
Mohit Jain <70198503+codedog001@users.noreply.github.com>
2021-08-20 17:35:33 +0530

    Heroku Deployment

--- lib/constants/strings.dart
+++ lib/constants/strings.dart
@@ -3,7 +3,5 @@
 //   return 'http://10.0.2.2:8000/menus/$id/delete';
 // }
 
-const String BASE_URL = 'http://192.168.0.107:8000/menus/?format=json';
-String deleteUrl(id) {
-  return 'http://192.168.0.107:8000/menus/$id/delete';
-}
+const String BASE_URL =
+    'https://todays-tiffin.herokuapp.com/menus/?format=json';
